90596914eab9f43194b97412de03d8e99a44e62d
japleen-arora77 <arorajapleen.cs2277@gmail.com>
2026-04-18 00:54:09 +0530

    Database changed to Neon

--- backend/sync_backend/settings.py
+++ backend/sync_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+import dj_database_url
 from dotenv import load_dotenv
 from pathlib import Path
 from datetime import timedelta
@@ -94,16 +95,16 @@ WSGI_APPLICATION = 'sync_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv("DB_NAME"),
-            'USER': os.getenv("DB_USER"),
-            'PASSWORD': os.getenv("DB_PASSWORD"),
-            'HOST': os.getenv("DB_HOST"),
-            'PORT': os.getenv("DB_PORT"),
-            'CONN_MAX_AGE': 60,
-    }
+    'default': dj_database_url.parse(
+        os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+# if os.getenv("DB_HOST") != "localhost":
+#     DATABASES['default']['OPTIONS'] = {
+#         'sslmode': 'require',
+#     }
 
 
 # Password validation
